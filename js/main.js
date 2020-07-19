f34496477a0654505ad6d115113a3a8507b55241
Richard Lasso <lasso_1995@hotmail.com>
2020-07-19 18:36:46 -0500

    Finish schedule

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 // Variables
-const btnSpeakers = document.querySelector('#speakers'),
-    close = document.querySelector('#close'),
-    activePopup = document.querySelector('.active-popup'),
-    speakers = document.querySelector('.speakers-popup'),
+
+// speaker pop up
+
+let close = document.querySelector('#close'),
     body = document.querySelector('body');
+
+const btnSpeakers = document.querySelector('#speakers'),
+    activePopup = document.querySelector('.active-popup'),
+    speakers = document.querySelector('.speakers-popup');
+    
+
+// schedule pop up
+
+const btnSchedule = document.querySelector('#schedule'),
+schedule = document.querySelector('.schedule-popup');
 
 
 // Event listeners
+
+// speakers pop up
 
 btnSpeakers.addEventListener('click', (e) => {
     e.preventDefault();
@@ -26,6 +38,30 @@ close.addEventListener('click', (e) => {
         body.style.overflow = 'visible';
     }   else {
         speakers.classList.add('active-popup');
+        body.style.overflow = 'hidden';
+    }
+});
+
+// schedule pop up
+
+btnSchedule.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(schedule.classList.contains('active-popup')) {
+        schedule.classList.remove('active-popup');
+        body.style.overflow = 'visible';
+    }   else {
+        schedule.classList.add('active-popup');
+        body.style.overflow = 'hidden';
+    }
+});
+
+close.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(schedule.classList.contains('active-popup')) {
+        schedule.classList.remove('active-popup');
+        body.style.overflow = 'visible';
+    }   else {
+        schedule.classList.add('active-popup');
         body.style.overflow = 'hidden';
     }
 });
