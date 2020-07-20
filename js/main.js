ff162806e3962004402748b9ed56f8f4d16bd726
Richard Lasso <lasso_1995@hotmail.com>
2020-07-20 13:20:54 -0500

    Finished language pop up and finished footer

--- js/main.js
+++ js/main.js
@@ -1,27 +1,32 @@
 // Variables
 
-// speaker pop up
-
-let close = document.querySelector('#close'),
+let 
+    activePopup = document.querySelector('.active-popup'),
     body = document.querySelector('body');
 
-const btnSpeakers = document.querySelector('#speakers'),
-    activePopup = document.querySelector('.active-popup'),
-    speakers = document.querySelector('.speakers-popup');
+
+const 
+    btnSpeakers = document.querySelector('#speakers'),
+    btnSchedule = document.querySelector('#schedule'),
+    btnFiles = document.querySelector('#files'),
+    btnLanguage = document.querySelector('#language'),
     
+    speakers = document.querySelector('.speaker'),
+    schedule = document.querySelector('.schedule'),
+    files = document.querySelector('.files'),
+    language = document.querySelector('.language'),
 
-// schedule pop up
-
-const btnSchedule = document.querySelector('#schedule'),
-schedule = document.querySelector('.schedule-popup');
-
+    close1 = document.querySelector('#close-1'),
+    close2 = document.querySelector('#close-2'),
+    close3 = document.querySelector('#close-3'),
+    close4 = document.querySelector('#close-4');
 
 // Event listeners
 
 // speakers pop up
 
-btnSpeakers.addEventListener('click', (e) => {
-    e.preventDefault();
+btnSpeakers.addEventListener('click', (speaker) => {
+    speaker.preventDefault();
     if(speakers.classList.contains('active-popup')) {
         speakers.classList.remove('active-popup');
         body.style.overflow = 'visible';
@@ -31,8 +36,8 @@ btnSpeakers.addEventListener('click', (e) => {
     }
 });
 
-close.addEventListener('click', (e) => {
-    e.preventDefault();
+close1.addEventListener('click', (speaker) => {
+    speaker.preventDefault();
     if(speakers.classList.contains('active-popup')) {
         speakers.classList.remove('active-popup');
         body.style.overflow = 'visible';
@@ -55,7 +60,7 @@ btnSchedule.addEventListener('click', (e) => {
     }
 });
 
-close.addEventListener('click', (e) => {
+close2.addEventListener('click', (e) => {
     e.preventDefault();
     if(schedule.classList.contains('active-popup')) {
         schedule.classList.remove('active-popup');
@@ -66,4 +71,50 @@ close.addEventListener('click', (e) => {
     }
 });
 
+// files pop up
 
+btnFiles.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(files.classList.contains('active-popup')) {
+        files.classList.remove('active-popup');
+        body.style.overflow = 'visible';
+    }   else {
+        files.classList.add('active-popup');
+        body.style.overflow = 'hidden';
+    }
+});
+
+close3.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(files.classList.contains('active-popup')) {
+        files.classList.remove('active-popup');
+        body.style.overflow = 'visible';
+    }   else {
+        files.classList.add('active-popup');
+        body.style.overflow = 'hidden';
+    }
+});
+
+// language pop up
+
+btnLanguage.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(language.classList.contains('active-popup')) {
+        language.classList.remove('active-popup');
+        body.style.overflow = 'visible';
+    }   else {
+        language.classList.add('active-popup');
+        body.style.overflow = 'hidden';
+    }
+});
+
+close4.addEventListener('click', (e) => {
+    e.preventDefault();
+    if(language.classList.contains('active-popup')) {
+        language.classList.remove('active-popup');
+        body.style.overflow = 'visible';
+    }   else {
+        language.classList.add('active-popup');
+        body.style.overflow = 'hidden';
+    }
+});
