89e6d2da4f163b87de843fd5adada2c540b8a465
Richard Lasso <lasso_1995@hotmail.com>
2020-07-22 19:13:20 -0500

    finished event page

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
+// FOOTER POP UP
+
 let 
     body = document.querySelector('body');
 
 const
     btnFooter = document.querySelectorAll('.btn--footer'),
-    close = document.querySelectorAll('.close')
-
-    console.log(btnFooter);
+    close = document.querySelectorAll('.close');
 
 btnFooter.forEach(item => {
   item.addEventListener('click', event => {
@@ -25,3 +25,41 @@ close.forEach(item => {
     })
   })
 });
+
+// MEDIA POP UP
+
+const
+    btnPopup = document.querySelectorAll('.btn-popup'),
+    closeMedia = document.querySelectorAll('.close-media');;
+
+    console.log(btnPopup);
+
+btnPopup.forEach(item => {
+  item.addEventListener('click', event => {
+    let index = Array.prototype.slice.call(item.parentElement.children).indexOf(item);
+    let popup = document.querySelectorAll('.media__popup');
+    popup[index].classList.add('active-popup--media');
+    body.style.overflow = 'hidden';
+  })
+});
+
+closeMedia.forEach(item => {
+  item.addEventListener('click', event => {
+    let popup = document.querySelectorAll('.media__popup');
+    popup.forEach(item => {
+      item.classList.remove('active-popup--media');
+      body.style.overflow = 'visible';
+    })
+  })
+});
+
+
+// Box slider
+
+$('.bxslider').bxSlider({
+    slideMargin: 10,
+    minSlides: 4,
+    maxSlides: 10,
+    slideWidth: 300,
+    
+  });
